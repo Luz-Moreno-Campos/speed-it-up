aaad407e3733f24fbe68d6aed59110dfa9103252
Luz Moreno Campos <luzdirection@gmail.com>
2026-04-07 11:19:49 -0500

    created start function

--- scripts/script.js
+++ scripts/script.js
@@ -2,31 +2,46 @@
 
 import { getElement, select, listen } from './utils.js';
 
-const targetWord = getElement('#current-word');
+const targetWord = getElement('current-word');
+const inputField = getElement('word-input');
+const startBtn = getElement('start-btn');
 
-const words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 
-  'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money', 
-  'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow', 
-  'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer', 
-  'philosophy', 'database', 'periodic', 'capitalism', 'abominable', 'component', 
-  'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty', 
-  'agency', 'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician', 
-  'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 'evolution', 'banana', 
-  'perfumer', 'computer', 'management', 'discovery', 'ambition', 'music', 'eagle', 
-  'crown', 'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'superman', 
-  'library', 'unboxing', 'bookstore', 'language', 'homework', 'fantastic', 'economy', 
-  'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous', 'league', 'memory', 
+const words = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
+  'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
+  'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
+  'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
+  'philosophy', 'database', 'periodic', 'capitalism', 'abominable', 'component',
+  'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada', 'coffee', 'beauty',
+  'agency', 'chocolate', 'eleven', 'technology', 'alphabet', 'knowledge', 'magician',
+  'professor', 'triangle', 'earthquake', 'baseball', 'beyond', 'evolution', 'banana',
+  'perfumer', 'computer', 'management', 'discovery', 'ambition', 'music', 'eagle',
+  'crown', 'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'superman',
+  'library', 'unboxing', 'bookstore', 'language', 'homework', 'fantastic', 'economy',
+  'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous', 'league', 'memory',
   'leather', 'planet', 'software', 'update', 'yellow', 'keyboard', 'window'];
 
 
-  const getRandomWord = function(words) {
+inputField.disabled =true; 
 
-    const nextWordIndex = Math.floor(Math.random() * words.length)
-    return words.splice(nextWordIndex, 1)[0];
-  }
+const getRandomWord = function (words) {
+  const nextWordIndex = Math.floor(Math.random() * words.length)
+  return words.splice(nextWordIndex, 1)[0];
+}
 
-  const displayWord = function() {
-    const nextWord = getRandomWord(words);
-    targetWord.innerText = nextWord;
+const displayWord = function () {
+  const nextWord = getRandomWord(words);
+  targetWord.innerText = nextWord;
+}
 
-  }
+const startGame = function() {
+  displayWord();       
+  inputField.value = '';
+  inputField.disabled = false; 
+  inputField.focus();  // this is to put the cursor in the input field   
+}
+
+listen('click', startBtn, startGame);
+
+
+
+
